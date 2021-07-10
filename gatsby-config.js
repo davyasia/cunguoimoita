21bd69a1816273e7c0eb0359287afa7dc9afbf82
Tiến Đạt Trần <trantiendat016@gmail.com>
2021-07-10 15:10:51 +0700

    feat: Replace source code by initial gatsby project (google analytics, netlify cms, emotion js)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,77 +1,44 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    siteUrl: "https://www.yourdomain.tld",
+    title: "cunguoimoita",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-image",
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
+        trackingId: "278786742",
       },
     },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
+        icon: "src/images/icon.png",
       },
     },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
+        name: "images",
+        path: "./src/images/",
       },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              destinationDir: 'static',
-            },
-          },
-        ],
-      },
+      __key: "images",
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-source-filesystem",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        name: "pages",
+        path: "./src/pages/",
       },
+      __key: "pages",
     },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-}
+};
