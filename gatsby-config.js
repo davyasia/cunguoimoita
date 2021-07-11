71658ca2b4a8e49d30b7c2a368cedf038ce62aa2
Tiến Đạt Trần <trantiendat016@gmail.com>
2021-07-11 20:01:51 +0700

    feat: Add netlify cms configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,12 @@ module.exports = {
     title: "cunguoimoita",
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     "gatsby-plugin-emotion",
     {
       resolve: 'gatsby-plugin-material-ui',
